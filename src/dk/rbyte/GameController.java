90a48ffaad690f9f1043642de13c24a823c241cc
Lizator <frederikkoefoeds@gmail.com>
2021-02-17 10:51:20 +0100

    implemented errors costing moves

--- src/dk/rbyte/GameController.java
+++ src/dk/rbyte/GameController.java
@@ -18,46 +18,55 @@ public class GameController {
 
     public boolean executeCommand(String command) throws Exception {
         String[] exe = command.split(" ");
-        switch (exe[0]){
-            case "move": case "m":
-                if (exe.length == 3){
-                    String x = exe[1];
-                    String y = exe[2];
-                    if (gameBoard.move(x, y)){ //will be false if a king from a king move is tried to be moved
-                        score += 5;
+        try {
+            switch (exe[0]) {
+                case "move":
+                case "m":
+                    if (exe.length == 3) {
+                        String x = exe[1];
+                        String y = exe[2];
+                        if (gameBoard.move(x, y)) { //will be false if a king from a king move is tried to be moved
+                            score += 5;
+                        }
+                        turns += 1;
+                        return true;
+                    } else if (exe.length == 4) {
+                        String x = exe[1];
+                        String y = exe[2];
+                        String n = exe[3];
+                        gameBoard.move(x, y, n);
+                        turns += 1;
+                        return true;
                     }
-                    turns += 1;
+                    break;
+                case "draw":
+                case "d":
+                    if (gameBoard.draw()) turns += 1;
                     return true;
-                } else if (exe.length == 4) {
-                    String x = exe[1];
-                    String y = exe[2];
-                    String n = exe[3];
-                    gameBoard.move(x, y, n);
-                    turns += 1;
-                    return true;
-                }
-                break;
-            case "draw": case "d":
-                if (gameBoard.draw()) turns += 1;
-                return true;
-            case "score": case "s":
-                if (exe.length == 2) {
-                    if (gameBoard.score(exe[1])){
-                        score += 10;
+                case "score":
+                case "s":
+                    if (exe.length == 2) {
+                        if (gameBoard.score(exe[1])) {
+                            score += 10;
+                        }
+                        turns += 1;
+                        return true;
                     }
-                    turns += 1;
-                    return true;
-                }
-                break;
-            case "return": case "r":
-                if (exe.length == 3){
-                    if (gameBoard.remove(exe[1], exe[2])){
-                        score -= 10;
+                    break;
+                case "return":
+                case "r":
+                    if (exe.length == 3) {
+                        if (gameBoard.remove(exe[1], exe[2])) {
+                            score -= 10;
+                        }
+                        turns += 1;
+                        return true;
                     }
-                    turns += 1;
-                    return true;
-                }
-                break;
+                    break;
+            }
+        } catch (Exception e) {
+            turns += 1;
+            throw e;
         }
 
         throw new Exception("command not found");
